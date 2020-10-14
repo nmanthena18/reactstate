151109580e92760c5fb3d99758d71a1597afa6b5
Naresh Manthena <nareshm6@birlasoft.com>
2020-10-14 19:59:38 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [state, updateState] = React.useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+        <input test-id="name" type="text" value={state} onChange={(event) => updateState(event.target.value)} />        
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {shallow} from 'enzyme';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />)
+  const updateState = jest.fn();
+  React.useState = jest.fn(() => ["", updateState]);
+  const input = wrapper.find('[test-id="name"]');
+  const event = {target: {value: 'test'}};
+  input.simulate('change', event);
+  expect(updateState).toHaveBeenCalledWith("test")
 });
